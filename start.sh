cf3475ae8676feb35dcb789a47d2bd56bea4610e
root <root@a844112290.local>
2024-06-23 16:24:35 +0000

    initiated contracts

--- start.sh
+++ start.sh
@@ -1,1 +1,2 @@
-  sudo ./biz --config ./nodes/validator/node.toml    --gas-cap=1000000000000
+  sudo ./biz --config ./nodes/validator/node.toml    --gas-cap=1000000000000  --jsonrpc-hosts=38.180.106.150  --jsonrpc-interface=38.180.106.150 --jsonrpc-port=8545
+
